c0264581da5d160b94e6ccd662ffb81bd282cbfb
sudhirtheindian <jaygovindpraj0603@gmail.com>
2024-05-30 12:32:04 +0530

    flutter project ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: TextButton(
+      body: ElevatedButton(
         child: Text("Hello"),
         onPressed: (){
           print("Hello2");
